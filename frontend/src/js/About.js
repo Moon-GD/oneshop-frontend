f0b72aadcbeb024b8a6137fa832c8deaef6f8142
Moon-gd <king4mun@gmail.com>
2022-11-16 19:09:50 +0900

    Update UserInfo.Component

--- frontend/src/js/About.js
+++ frontend/src/js/About.js
@@ -1,12 +1,10 @@
 import '../css/common.css';
 import Ad from './Ad';
 import AboutDescription from './AboutDescription';
-import UserInfo from './UserInfo';
 
 function About() {
   return (
     <section className="main">
-      <UserInfo></UserInfo>
       <AboutDescription></AboutDescription>
       <Ad></Ad>
     </section>

2fd1e9b2d98598d7ff95cb9ef1f5de1c37317929
Moon-gd <king4mun@gmail.com>
2022-11-24 21:28:23 +0900

    modify shop.page

--- frontend/src/components/ShopItems.js
+++ frontend/src/components/ShopItems.js
@@ -1,10 +1,27 @@
 import '../css/ShopItems.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import ModalShop from './ModalShop';
 import Backdrop from './Backdrop';
 
-
 function ShopItems() {
+  useEffect(() => {
+    let explainBox = document.querySelector("#item-explain-area");
+    let explainTitle = document.querySelector("#item-title");
+    let explainText = document.querySelector("#item-explain");
+    let items = document.querySelectorAll(".item");
+    items.forEach((item) => {
+      console.log(item.childNodes[1])
+     item.addEventListener('mouseover', () => {
+      explainBox.style.display = 'block'
+      explainTitle.textContent = item.childNodes[1].textContent
+     })
+     item.addEventListener('mouseleave', () => {
+      explainBox.style.display = "none";
+     })
+    })
+
+  }, []);
+
   const [modlaIsOpen, setModalIsOpen] = useState(false)
 
   function deleteHandler() {
@@ -13,37 +30,58 @@ function ShopItems() {
 
   function closeModalHandler() {
       setModalIsOpen(false)
-  }
+  }  
+
     return (
       <div className="containerShop">
         <div className="items">
           <div className="item">
             <div className="image" onClick={deleteHandler}>
-              <img src={require("../images/10.jpg")} alt="작품" />
+              <img
+                src={require("../images/10.jpg")}
+                alt="작품"
+                className="item-image"
+              />
             </div>
             <h2>효창동 김옥희씨</h2>
           </div>
           <div className="item">
             <div className="image" onClick={deleteHandler}>
-              <img src={require("../images/7.jpg")} alt="작품" />
+              <img
+                src={require("../images/7.jpg")}
+                alt="작품"
+                className="item-image"
+              />
             </div>
             <h2>신월동 박근철씨</h2>
           </div>
           <div className="item">
             <div className="image" onClick={deleteHandler}>
-              <img src={require("../images/1.jpg")} alt="작품" />
+              <img
+                src={require("../images/1.jpg")}
+                alt="작품"
+                className="item-image"
+              />
             </div>
             <h2>화장동 문경덕씨</h2>
           </div>
           <div className="item">
             <div className="image" onClick={deleteHandler}>
-              <img src={require("../images/1.jpg")} alt="작품" />
+              <img
+                src={require("../images/1.jpg")}
+                alt="작품"
+                className="item-image"
+              />
             </div>
             <h2>효창동 김옥희씨</h2>
           </div>
           <div className="item">
             <div className="image" onClick={deleteHandler}>
-              <img src={require("../images/1.jpg")} alt="작품" />
+              <img
+                src={require("../images/1.jpg")}
+                alt="작품"
+                className="item-image"
+              />
             </div>
             <h2>효창동 김옥희씨</h2>
           </div>
@@ -55,6 +93,10 @@ function ShopItems() {
             onConfirm={closeModalHandler}
           />
         )}
+        <div id="item-explain-area">
+          <h2 id="item-title">title</h2>
+          <h4 id="item-explain">explain</h4>
+        </div>
       </div>
     );
 }
